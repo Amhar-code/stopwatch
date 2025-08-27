f19a85f21858ad7093933dc7c76b4d1247687262
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-08-27 23:36:29 +0530

    Improved the CSS

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -3,28 +3,47 @@ import React,{useState, useEffect, useRef, use} from "react";
 function StopWatch() {
     const [isRunning, setIsRunning] = useState(false);
     const [elapsedTime, setElapsedTime] = useState(0);
-    const timerRef = useRef(null);
-    const startTimer = useRef(0);
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
 
     useEffect(() => {
+        if(isRunning){
+            intervalIdRef.current =  setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current);
+            }, 10);
+        }
 
+        return () => {
+            clearInterval(intervalIdRef.current);
+        }
     }, [isRunning]);
 
     function handleStart() {
-        
+       setIsRunning(true); 
+       startTimeRef.current = Date.now() - elapsedTime;
     }
 
     function handleStop() {
-        
+        setIsRunning(false);
     }
 
     function handleReset() {
-        
+        setElapsedTime(0);
+        setIsRunning(false);
     }       
 
-    function formatTime(ms) {
+    function formatTime() {
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+        let seconds = Math.floor(elapsedTime / (1000) % 60);
+        let miliseconds = Math.floor(elapsedTime % 1000 / 10);
         
-        return `00:00:00`;
+        hours = String(hours).padStart(2, "00");
+        minutes = String(minutes).padStart(2, "00");
+        seconds = String(seconds).padStart(2, "00");
+        miliseconds = String(miliseconds).padStart(2, "00");
+
+        return `${minutes}:${seconds}:${miliseconds}`;
     }
     return (
         <div className="stopwatch">
